ddacc71aa7a3a2e6358a65572c53187876ae98e6
Sanjay Nishad <sanjay120890@gmail.com>
2023-07-06 22:36:04 +0530

    feat: adding email-validator tool web component

---
+++ src/email-validator/email-validator.ts
@@ -0,0 +1,37 @@
+import { html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { inputStyles } from "../_styles/input.css.js"
+import { IConfigBase, WebComponentBase } from '../_web-component/WebComponentBase.js';
+
+@customElement('email-validator')
+export class EmailValidator extends WebComponentBase<IConfigBase> {
+    static override styles = [inputStyles];
+
+    @property()
+    isValid = false;
+
+    onChange(e: any) {
+        this.isValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(e.target?.value);
+    }
+
+    override render() {
+        return html`
+        <div style="display: flex; padding: 0.5rem 0;">
+            <input
+                name="email"
+                autofocus
+                placeholder="Enter email to validate"
+                @keyup=${this.onChange}
+                />
+        </div>
+        <div>
+            <strong>${this.isValid ? "Valid" : "Invalid"}</strong>
+        </div>`;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'email-validator': EmailValidator;
+    }
+}
