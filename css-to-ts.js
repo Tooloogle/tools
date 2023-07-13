8de97c32883c9af5c3b02b057d47e2c957198ac8
Sanjay Kumar Nishad <sanjaynishad@users.noreply.github.com>
2023-07-13 19:58:59 +0530

    tailwind css setup (#11)

--- css-to-ts.js
+++ css-to-ts.js
@@ -1,14 +1,30 @@
 import fs from "fs";
 import { globSync } from "glob";
 import path from "path";
+import postcss from "postcss";
+import tailwindcss from "tailwindcss";
+import tailwindcssForm from "@tailwindcss/forms";
+import cssnanoPlugin from "cssnano";
 
 const cssFiles = globSync("./src/**/*.css");
 
-cssFiles.forEach(cssFile => {
+cssFiles.forEach(async cssFile => {
     try {
         const fileName = path.parse(cssFile).name;
-        const content = fs.readFileSync(cssFile);
+        const filenameWithoutExtension = path.parse(cssFile).name;
+        let content = fs.readFileSync(cssFile);
         const variableName = `${fileName.replace(/-./g, val => val[1].toUpperCase())}Styles`;
+
+        // tailwindcss and minify css
+        content = await postcss([tailwindcss({
+            config: {
+                // must use same name for css and ts file and tool folder name for the component
+                content: [`./src/${filenameWithoutExtension}/${filenameWithoutExtension}.ts`],
+                plugins: [tailwindcssForm]
+            }
+        }), cssnanoPlugin]).process(content, { from: cssFile });
+
+        // write .css.ts file
         fs.writeFileSync(`${cssFile}.ts`, `
 // THIS IS AUTO GENERATED FILE, DO NOT MAKE ANY CHANGES HERE.
 import { css } from 'lit';
