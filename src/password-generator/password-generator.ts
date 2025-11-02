02a3185ff8060f8e1f236f83823aa3f530f3bc25
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2025-11-02 12:01:54 +0530

    chore: reviewed all tools and resolved password generator issue

--- src/password-generator/password-generator.ts
+++ src/password-generator/password-generator.ts
@@ -1,65 +1,80 @@
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import inputStyles from "../_styles/input.css.js"
-import { IConfigBase, WebComponentBase } from '../_web-component/WebComponentBase.js';
+import inputStyles from '../_styles/input.css.js';
+import {
+  IConfigBase,
+  WebComponentBase,
+} from '../_web-component/WebComponentBase.js';
 import buttonStyles from '../_styles/button.css.js';
 import passwordGeneratorStyles from './password-generator.css.js';
 import { when } from 'lit/directives/when.js';
-import "../t-copy-button/t-copy-button.js";
+import '../t-copy-button/t-copy-button.js';
 
-const passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const passwordChars =
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
 @customElement('password-generator')
 export class PasswordGenerator extends WebComponentBase<IConfigBase> {
-    static override styles = [WebComponentBase.styles, inputStyles, buttonStyles, passwordGeneratorStyles];
+  static override styles = [
+    WebComponentBase.styles,
+    inputStyles,
+    buttonStyles,
+    passwordGeneratorStyles,
+  ];
 
-    @property()
-    length = 10;
+  @property()
+  length = 10;
 
-    @property()
-    password = "";
+  @property()
+  password = '';
 
-    generate() {
-        this.password = Array(Number(this.length)).fill(passwordChars).map((x) => { return x[(Math.floor(Math.random() * passwordChars.length))] }).join('');
-    }
+  generate() {
+    this.password = Array(Number(this.length))
+      .fill(passwordChars)
+      .map(x => {
+        return x[Math.floor(Math.random() * passwordChars.length)];
+      })
+      .join('');
+  }
 
-    private handleLengthChange(e: Event) {
-        const target = e.target as HTMLInputElement;
-        this.length = Number(target?.value);
-    }
+  private handleLengthChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    this.length = Number(target?.value);
+  }
 
-    private renderPasswordWithCopyButton() {
-        return html`
-        <strong class="break-all">${this.password}</strong>
-        <t-copy-button .text=${this.password}></t-copy-button>
-        `;
-    }
+  private renderPasswordWithCopyButton = () => {
+    return html`
+      <strong class="break-all">${this.password}</strong>
+      <t-copy-button .text=${this.password}></t-copy-button>
+    `;
+  };
 
-    override render() {
-        return html`
-        <label class="block">
-            <span class="inline-block py-1">Password length</span>
-            <input
-                name="email"
-                class="form-input text-end"
-                type="number"
-                max="100"
-                autofocus
-                .value=${this.length}
-                @keyup=${this.handleLengthChange}
-                />
-        </label>
-        <div class="py-2 flex content-center">
-            ${when(this.password, this.renderPasswordWithCopyButton)}
-        </div>
-        <div class="text-end">
-            <button class="btn btn-blue" @click=${this.generate}>Generate Strong Password</button>
-        </div>`;
-    }
+  override render() {
+    return html` <label class="block">
+        <span class="inline-block py-1">Password length</span>
+        <input
+          name="email"
+          class="form-input text-end"
+          type="number"
+          max="100"
+          autofocus
+          .value=${this.length}
+          @keyup=${this.handleLengthChange}
+        />
+      </label>
+      <div class="py-2 flex content-center">
+        ${when(this.password, this.renderPasswordWithCopyButton)}
+      </div>
+      <div class="text-end">
+        <button class="btn btn-blue" @click=${this.generate}>
+          Generate Strong Password
+        </button>
+      </div>`;
+  }
 }
 
 declare global {
-    interface HTMLElementTagNameMap {
-        'password-generator': PasswordGenerator;
-    }
+  interface HTMLElementTagNameMap {
+    'password-generator': PasswordGenerator;
+  }
 }
